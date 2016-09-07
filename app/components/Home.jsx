aaa109fde7eaa640dbda58de1e44ddfd4575eaa5
Vincent Navetat <vincentnavetat@gmail.com>
2016-09-07 14:38:30 +0100

    Added preview kashflow

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -14,16 +14,22 @@ const Home = React.createClass({
           <h1>Selected Projects</h1>
           <div className="project-cards">
             <Link to={'taskly'} className="project-card">
-              <h2 className="project-card__name">Taskly</h2>
+              <h2 className="project-card__overlay project-card__name">Taskly</h2>
+              <p className="project-card__overlay project-card__description">Project Management App Concept and Design</p>
+              <p className="project-card__overlay project-card__skillset">UX/UI Design</p>
               <img className="project-card__image" src="./medias/taskly/preview.jpg" alt="Taskly" />
             </Link>
             <Link to={'ux-ui-design'} className="project-card">
-              <h2 className="project-card__name">UX/UI Design</h2>
+              <h2 className="project-card__overlay project-card__name">UX/UI Design</h2>
+              <p className="project-card__overlay project-card__description">Mobile Apps Design</p>
+              <p className="project-card__overlay project-card__skillset">UX/UI Design</p>
               <img className="project-card__image" src="./medias/ux-ui-design/preview.jpg" alt="UX/UI Design" />
             </Link>
             <Link to={'kashflow'} className="project-card">
-              <h2 className="project-card__name">KashFlow</h2>
-              <img className="project-card__image" src="./medias/ux-ui-design/preview.jpg" alt="UX/UI Design" />
+              <h2 className="project-card__overlay project-card__name">KashFlow</h2>
+              <p className="project-card__overlay project-card__description">Accounting Software Website</p>
+              <p className="project-card__overlay project-card__skillset">Web Development</p>
+              <img className="project-card__image" src="./medias/kashflow/preview.jpg" alt="KashFlow" />
             </Link>
           </div>
           <Footer />
